7bb60ec888ba11b71b73c9462020e86a461e4644
Shinsuke Sugaya <shinsuke@apache.org>
2017-10-08 11:36:51 +0900

    wait for elasticsearch startup

--- 11.3/run.sh
+++ 11.3/run.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-service elasticsearch start
-service fess start
-
 ES_LOGFILE=/var/log/elasticsearch/elasticsearch.log
-FESS_LOGFILE=/var/log/fess/fess.log
 
-while [ ! -f ${ES_LOGFILE} ] || [ ! -f ${FESS_LOGFILE} ]
-do
-  sleep 1s
+service elasticsearch start
+
+counter=1
+ret=1
+while [ $ret != 0 -a $counter -le 60 ] ; do
+  echo "Checking Elasticsearch status #$counter"
+  grep "] started$" $ES_LOGFILE > /dev/null 2>&1
+  ret=$?
+  counter=`expr $counter + 1`
+  sleep 1
 done
 
-tail -f ${ES_LOGFILE} -f ${FESS_LOGFILE}
+service fess start
+
+tail -f ${ES_LOGFILE} /var/log/fess/*.log
