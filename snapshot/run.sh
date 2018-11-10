51fc24327119e6d92a770010e5b2c35b648d53d7
Shinsuke Sugaya <shinsuke@apache.org>
2018-11-10 15:27:33 +0900

    add USE_EXTERNAL_ELASTICSEARCH

--- snapshot/run.sh
+++ snapshot/run.sh
@@ -1,26 +1,45 @@
 #!/bin/bash
 
 ES_LOGFILE=/var/log/elasticsearch/elasticsearch.log
-ES_HOST=localhost:9200
 
-if [ x"$ES_JAVA_OPTS" != "x" ] ; then
-  echo "ES_JAVA_OPTS=$ES_JAVA_OPTS" >> /etc/default/elasticsearch
+if [ x"$ES_HTTP_URL" = "x" ] ; then
+  ES_HTTP_URL=http://localhost:9200
+else
+  sed -i -e "s/^ES_HTTP_URL=.*/ES_HTTP_URL=$ES_HTTP_URL/" /etc/default/fess
 fi
-service elasticsearch start
 
-counter=1
-ret=1
-while [ $ret != 0 -a $counter -le 60 ] ; do
-  echo "Checking Elasticsearch status #$counter"
-  grep "started" $ES_LOGFILE > /dev/null 2>&1
-  ret=$?
-  counter=`expr $counter + 1`
-  sleep 1
-done
+if [ x"$ES_TRANSPORT_URL" = "x" ] ; then
+  ES_TRANSPORT_URL=localhost:9300
+else
+  sed -i -e "s/^ES_TRANSPORT_URL=.*/ES_TRANSPORT_URL=$ES_TRANSPORT_URL/" /etc/default/fess
+fi
 
-cat ${ES_LOGFILE}
-tail -f ${ES_LOGFILE} &
-curl -XGET "$ES_HOST/_cluster/health?wait_for_status=yellow&timeout=3m"
+start_elasticsearch() {
+  touch $ES_LOGFILE
+  chown elasticsearch:elasticsearch $ES_LOGFILE
+  tail -f ${ES_LOGFILE} &
+
+  if [ x"$ES_JAVA_OPTS" != "x" ] ; then
+    echo "ES_JAVA_OPTS=$ES_JAVA_OPTS" >> /etc/default/elasticsearch
+  fi
+  service elasticsearch start
+
+  counter=1
+  ret=1
+  while [ $ret != 0 -a $counter -le 60 ] ; do
+    echo "Checking Elasticsearch status #$counter"
+    grep "started" $ES_LOGFILE > /dev/null 2>&1
+    ret=$?
+    counter=`expr $counter + 1`
+    sleep 1
+  done
+
+  curl -XGET "$ES_HTTP_URL/_cluster/health?wait_for_status=yellow&timeout=3m"
+}
+
+if [ x"$USE_EXTERNAL_ELASTICSEARCH" != "xtrue" ] ; then
+  start_elasticsearch
+fi
 
 touch /var/log/fess/fess-crawler.log /var/log/fess/fess-suggest.log \
       /var/log/fess/fess-thumbnail.log \
