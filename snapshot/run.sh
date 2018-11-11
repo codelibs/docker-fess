be39d4d0dff977ed7d01475ffdff08cbbd503ca0
Shinsuke Sugaya <shinsuke@apache.org>
2018-11-11 21:57:57 +0900

    add wait_app

--- snapshot/run.sh
+++ snapshot/run.sh
@@ -14,6 +14,14 @@ else
   sed -i -e "s/^ES_TRANSPORT_URL=.*/ES_TRANSPORT_URL=$ES_TRANSPORT_URL/" /etc/default/fess
 fi
 
+if [ x"$PING_RETRIES" = "x" ] ; then
+  PING_RETRIES=3
+fi
+
+if [ x"$PING_INTERVAL" = "x" ] ; then
+  PING_INTERVAL=60
+fi
+
 start_elasticsearch() {
   touch $ES_LOGFILE
   chown elasticsearch:elasticsearch $ES_LOGFILE
@@ -37,20 +45,61 @@ start_elasticsearch() {
   curl -XGET "$ES_HTTP_URL/_cluster/health?wait_for_status=yellow&timeout=3m"
 }
 
-if [ x"$USE_EXTERNAL_ELASTICSEARCH" != "xtrue" ] ; then
+start_fess() {
+  touch /var/log/fess/fess-crawler.log \
+        /var/log/fess/fess-suggest.log \
+        /var/log/fess/fess-thumbnail.log \
+        /var/log/fess/audit.log \
+	/var/log/fess/fess.log
+  chown fess:fess /var/log/fess/fess-crawler.log \
+                  /var/log/fess/fess-suggest.log \
+                  /var/log/fess/fess-thumbnail.log \
+                  /var/log/fess/audit.log \
+		  /var/log/fess/fess.log
+  tail -f /var/log/fess/*.log &
+  service fess start
+}
+
+wait_app() {
+  while [ 1 ] ; do
+    if [ x"$RUN_ELASTICSEARCH" != "xfalse" ] ; then
+      STATUS=`curl -w '%{http_code}\n' -s -o /dev/null http://localhost:9200`
+      if [ x"$STATUS" = x200 ] ; then
+        ELASTICSEARCH_ERROR_COUNT=0
+      else
+        ELASTICSEARCH_ERROR_COUNT=`expr $ELASTICSEARCH_ERROR_COUNT + 1`
+      fi
+      if [ $ELASTICSEARCH_ERROR_COUNT -ge $PING_RETRIES ] ; then
+        echo "Elasticsearch is not available."
+        exit 1
+      fi
+    fi
+    if [ x"$RUN_FESS" != "xfalse" ] ; then
+      STATUS=`curl -w '%{http_code}\n' -s -o /dev/null http://localhost:8080/json/ping`
+      if [ x"$STATUS" = x200 ] ; then
+        FESS_ERROR_COUNT=0
+      else
+        FESS_ERROR_COUNT=`expr $FESS_ERROR_COUNT + 1`
+      fi
+      if [ $FESS_ERROR_COUNT -ge $PING_RETRIES ] ; then
+        echo "Fess is not available."
+        exit 1
+      fi
+    fi
+    sleep $PING_INTERVAL
+  done
+}
+
+if [ x"$RUN_ELASTICSEARCH" != "xfalse" ] ; then
   start_elasticsearch
 fi
 
-touch /var/log/fess/fess-crawler.log /var/log/fess/fess-suggest.log \
-      /var/log/fess/fess-thumbnail.log \
-      /var/log/fess/audit.log  /var/log/fess/fess.log
-chown fess:fess /var/log/fess/fess-crawler.log /var/log/fess/fess-suggest.log \
-      /var/log/fess/fess-thumbnail.log \
-      /var/log/fess/audit.log  /var/log/fess/fess.log
-service fess start
+if [ x"$RUN_FESS" != "xfalse" ] ; then
+  start_fess
+fi
 
 if [ x"$RUN_SHELL" = "xtrue" ] ; then
   /bin/bash
 else
-  tail -fq /var/log/fess/*.log
+  wait_app
 fi
